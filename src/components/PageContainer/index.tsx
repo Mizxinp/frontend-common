e0d4eaae9d74d522dfad76ded53f315f2405ae06
mizxinp <mizxinp@gmail.com>
2021-11-03 22:22:46 +0800

    feat: add Panel component

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Layout, Menu } from 'antd';
+import { useHistory } from 'react-router-dom';
 import styles from './index.module.scss';
 
-const { Header, Content, Footer } = Layout;
+const { Header } = Layout;
 
 function PageContainer(props: { children: React.ReactChild }) {
+  const history = useHistory();
   return (
     <div>
       <Layout>
         <Header>
           <Menu theme="dark" mode="horizontal">
-            <Menu.Item>图表</Menu.Item>
-            <Menu.Item>其他</Menu.Item>
+            <Menu.Item onClick={() => history.push('/')}>图表</Menu.Item>
+            <Menu.Item onClick={() => history.push('/other')}>其他</Menu.Item>
           </Menu>
         </Header>
       </Layout>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Panel from '../../components/Panel';
 
 
 function Home() {
   const history = useHistory()
   return (
-    <div onClick={() => history.push('/other')}>首页</div>
+    <div onClick={() => history.push('/other')}>
+      <Panel title="饼图">
+        这是饼图
+      </Panel>
+    </div>
   )
 }
 
