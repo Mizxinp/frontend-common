2a7e37df153aace3cffb3ebb9bc4fd880f32f30a
mizxinp <mizxinp@gmail.com>
2021-11-04 22:50:32 +0800

    feat:  run eject

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import Panel from '../../components/Panel';
+import Panel from '@/components/Panel';
+
+import styles from './index.module.scss';
 
 
 function Home() {
@@ -8,7 +10,7 @@ function Home() {
   return (
     <div onClick={() => history.push('/other')}>
       <Panel title="饼图">
-        这是饼图
+        <div className={styles.test}>这是饼图</div>
       </Panel>
     </div>
   )
